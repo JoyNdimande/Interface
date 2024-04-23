6be9fb58b2defbfbd49d2a941405b2a98870e5c8
JoyNdimande <keotshepilendimande@gmail.com>
2024-04-23 13:19:10 +0200

    Add project files.

---
+++ Interfaces 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Piano piano = new Piano();
+        Guitar guitar = new Guitar();
+         piano.Play();
+         guitar.Play();
+    }
+
+    public interface IPlayable
+    {
+        void Play();
+    }
+
+    public class Guitar : IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("GUITAR IS NOW PLAYING!");
+        }
+    }
+
+    public class Piano : IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("PIANO IS NOW PLAYING!");
+        }
+    }
+}
